f6fbe704e4ccb750808ec86cc33b19b0f855fed8
rickdeveloperProjects <97768287+rickdeveloperProjects@users.noreply.github.com>
2022-10-30 11:11:27 -0300

    Create Unid3Code2.py

---
+++ Unid3Code2.py
@@ -0,0 +1,22 @@
+#Unidade1Code2
+
+valorA = int(input("Digite o valor de A: "))
+valorB = int(input("Digite o valor de B: "))
+valorC = int(input("Digite o valor de C: "))
+
+print ("\nvalorA = {}, valorB = {}, valorC = {} ".format(valorA,valorB,valorC))
+
+if valorA > valorB + valorC:
+    print ('\nOs valores nao formam um triangulo pois A > B + C')
+
+else: 
+    print ('Os valores formam um triangulo')
+
+
+
+
+
+
+
+
+
